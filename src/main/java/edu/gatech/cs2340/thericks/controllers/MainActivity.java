2e2b833a735091d9535674bd0de471b32828ce33
CameronCacic <ccacic0@gmail.com>
2017-11-21 14:42:05 -0500

    Adds MainActivity to be launching point of program

---
+++ src/main/java/edu/gatech/cs2340/thericks/controllers/MainActivity.java
@@ -0,0 +1,12 @@
+package edu.gatech.cs2340.thericks.controllers;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainActivity extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+}

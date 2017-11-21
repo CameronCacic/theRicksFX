a94df23fe8b657c3f2ad79099eecfc4d6b9ced46
CameronCacic <ccacic0@gmail.com>
2017-11-21 14:42:53 -0500

    Converts DashMap activity to work in a JavaFX environment

--- src/main/java/edu/gatech/cs2340/thericks/controllers/DashMapActivity.java
+++ src/main/java/edu/gatech/cs2340/thericks/controllers/DashMapActivity.java
@@ -1,31 +1,27 @@
 package edu.gatech.cs2340.thericks.controllers;
 
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
-
+import java.io.IOException;
 import java.util.List;
 
-import edu.gatech.cs2340.thericks.R;
+import com.lynden.gmapsfx.GoogleMapView;
+import com.lynden.gmapsfx.MapComponentInitializedListener;
+import com.lynden.gmapsfx.javascript.object.GoogleMap;
+import com.lynden.gmapsfx.javascript.object.LatLong;
+import com.lynden.gmapsfx.javascript.object.MapOptions;
+import com.lynden.gmapsfx.javascript.object.MapTypeIdEnum;
+import com.lynden.gmapsfx.javascript.object.Marker;
+import com.lynden.gmapsfx.javascript.object.MarkerOptions;
+
 import edu.gatech.cs2340.thericks.database.RatDatabase;
 import edu.gatech.cs2340.thericks.models.RatData;
 import edu.gatech.cs2340.thericks.models.RatFilter;
 import edu.gatech.cs2340.thericks.models.User;
+import edu.gatech.cs2340.thericks.utils.Log;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * Created by Cameron on 10/6/2017.
@@ -33,176 +29,190 @@ import edu.gatech.cs2340.thericks.models.User;
  * to engage in. Defaults to dash mode, upon selecting map, dash-map switches to map mode,
  * where users can view rat data displayed on a Google Map, filtered by date
  */
-public class DashMapActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class DashMapActivity extends AnchorPane implements MapComponentInitializedListener {
 
-    private static final String TAG = DashMapActivity.class.getSimpleName();
-    private static final int ADD_RAT_DATA_REQUEST = 2;
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     //default position, zoom, and bearing to set the map camera to
-    private static final CameraPosition position = new CameraPosition(new LatLng(40.776278,
-            -73.99086), 12, 0, 30);
+    private static final LatLong POSITION = new LatLong(40.776278, -73.99086);
+    private static final double ZOOM = 12;
+    private static final double BEARING = 30;
 
+    private MapOptions staticMapOptions;
+    private MapOptions engagedMapOptions;
+    
     private GoogleMap map;
 
+    private RatFilter filter;
+    
+    private User user;
+    
     private boolean onMap;
 
+    @FXML
+    private GoogleMapView mapView;
+    
+    @FXML
     private Button mapButton;
+    
+    @FXML
+    private Button graphButton;
+    
+    @FXML
     private Button listRatDataButton;
+    
+    @FXML
     private Button profileButton;
+    
+    @FXML
     private Button settingsButton;
+    
+    @FXML
     private Button reportRatButton;
+    
+    @FXML
     private Button logoutButton;
 
+    @FXML
     private Button returnToDashButton;
+    
+    @FXML
     private Button applyFiltersButton;
 
-    private RatFilter filter;
-
+    @FXML
     private ProgressBar progressBar;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dash_map);
+    public DashMapActivity(User u) {
+    	user = u;
+    	
+    	staticMapOptions = new MapOptions();
+    	staticMapOptions.center(POSITION)
+    			.mapType(MapTypeIdEnum.ROADMAP)
+    			.overviewMapControl(false)
+    			.panControl(false)
+    			.rotateControl(false)
+    			.scaleControl(false)
+    			.streetViewControl(false)
+    			.zoomControl(false)
+    			.zoom(ZOOM);
+    	
+    	engagedMapOptions.center(POSITION)
+				.mapType(MapTypeIdEnum.ROADMAP)
+				.overviewMapControl(true)
+				.panControl(true)
+				.rotateControl(true)
+				.scaleControl(false)
+				.streetViewControl(false)
+				.zoomControl(true)
+				.zoom(ZOOM);
+    	
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource("activity_main.fxml"));
+    	loader.setController(this);
+    	loader.setRoot(this);
+		try {
+			loader.load();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    public void initialize() {
 
         Log.d(TAG, "Entered dashboard activity");
-
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-
-        mapButton = findViewById(R.id.map_button);
-        Button graphButton = findViewById(R.id.rat_graph_button);
-        listRatDataButton = findViewById(R.id.rat_data_list_button);
-        profileButton = findViewById(R.id.profile_button);
-        settingsButton = findViewById(R.id.settings_button);
-        reportRatButton = findViewById(R.id.report_rat_button);
-        logoutButton = findViewById(R.id.logout_button);
-
-        returnToDashButton = findViewById(R.id.return_to_dashboard_button);
-        returnToDashButton.setVisibility(View.GONE);
-
-        applyFiltersButton = findViewById(R.id.apply_filters_button);
-        applyFiltersButton.setVisibility(View.GONE);
 
         filter = RatFilter.getDefaultInstance();
 
         onMap = false;
 
-        progressBar = findViewById(R.id.progress_bar_dash_map);
-        progressBar.setVisibility(View.GONE);
-
-        Bundle b = getIntent().getExtras();
-        assert b != null;
-        User user = b.getParcelable("edu.gatech.cs2340.thericks.User");
         assert user != null;
         user.login();
-        Log.d(TAG, user.getLogin().getUsername() + " is logged in = " + user.isLoggedIn());
+        Log.d(TAG, user.getUsername() + " is logged in = " + user.isLoggedIn());
 
-        mapButton.setOnClickListener((View v) -> {
+        mapView.addMapInializedListener(this);
+        
+        mapButton.setOnAction(e -> {
             Log.d(TAG, "Rat Map Button pressed");
             if (map != null) {
-                mapButton.setVisibility(View.GONE);
-                listRatDataButton.setVisibility(View.GONE);
-                profileButton.setVisibility(View.GONE);
-                settingsButton.setVisibility(View.GONE);
-                reportRatButton.setVisibility(View.GONE);
-                logoutButton.setVisibility(View.GONE);
+                mapButton.setVisible(false);
+                listRatDataButton.setVisible(false);
+                profileButton.setVisible(false);
+                settingsButton.setVisible(false);
+                reportRatButton.setVisible(false);
+                logoutButton.setVisible(false);
 
-                loadFilteredMapMarkers();
-
-                returnToDashButton.setVisibility(View.VISIBLE);
-                applyFiltersButton.setVisibility(View.VISIBLE);
+                returnToDashButton.setVisible(true);
+                applyFiltersButton.setVisible(true);
 
                 onMap = !onMap;
-                map.getUiSettings().setAllGesturesEnabled(onMap);
+                map = mapView.createMap(engagedMapOptions);
+                map.setHeading(BEARING);
+                loadFilteredMapMarkers();
             }
         });
 
-        graphButton.setOnClickListener((View v) ->  {
+        graphButton.setOnAction(e ->  {
             Log.d(TAG, "Graph button pressed");
-            Context context = v.getContext();
-            Intent intent = new Intent(context, GraphActivity.class);
-            context.startActivity(intent);
+//            Context context = v.getContext();
+//            Intent intent = new Intent(context, GraphActivity.class);
+//            context.startActivity(intent);
         });
 
-        listRatDataButton.setOnClickListener((View v) -> {
+        listRatDataButton.setOnAction(e -> {
             Log.d(TAG, "Rat Data List button pressed");
-            Context context = v.getContext();
-            Intent intent = new Intent(context, RatDataListActivity.class);
-            context.startActivity(intent);
+//            Context context = v.getContext();
+//            Intent intent = new Intent(context, RatDataListActivity.class);
+//            context.startActivity(intent);
         });
 
-        profileButton.setOnClickListener((View v) -> {
+        profileButton.setOnAction(e -> {
             Log.d(TAG, "Profile button pressed");
-            Context context = v.getContext();
-            Intent intent = new Intent(context, FilterActivity.class);
-            context.startActivity(intent);
+//            Context context = v.getContext();
+//            Intent intent = new Intent(context, FilterActivity.class);
+//            context.startActivity(intent);
         });
 
-        reportRatButton.setOnClickListener((View v) -> {
+        reportRatButton.setOnAction(e -> {
             Log.d(TAG, "Report a Rat button pushed");
-            Context context = v.getContext();
-            Intent intent = new Intent(context, RatEntryActivity.class);
-            startActivityForResult(intent, ADD_RAT_DATA_REQUEST);
+//            Context context = v.getContext();
+//            Intent intent = new Intent(context, RatEntryActivity.class);
+//            startActivityForResult(intent, ADD_RAT_DATA_REQUEST);
         });
 
-        logoutButton.setOnClickListener(v -> {
+        logoutButton.setOnAction(e -> {
             user.logout();
             Log.d(TAG, "Logout button pressed");
             Log.d(TAG, user.getLogin().getUsername() + " is logged in = "
                     + user.isLoggedIn());
-            Context context = v.getContext();
-            Intent intent = new Intent(context, WelcomeActivity.class);
-            context.startActivity(intent);
-            finish();
+//            Context context = v.getContext();
+//            Intent intent = new Intent(context, WelcomeActivity.class);
+//            context.startActivity(intent);
+            //finish();
         });
 
-        returnToDashButton.setOnClickListener(v -> {
+        returnToDashButton.setOnAction(e -> {
             if (map != null) {
-                mapButton.setVisibility(View.VISIBLE);
-                listRatDataButton.setVisibility(View.VISIBLE);
-                profileButton.setVisibility(View.VISIBLE);
-                settingsButton.setVisibility(View.VISIBLE);
-                reportRatButton.setVisibility(View.VISIBLE);
-                logoutButton.setVisibility(View.VISIBLE);
+                mapButton.setVisible(true);
+                listRatDataButton.setVisible(true);
+                profileButton.setVisible(true);
+                settingsButton.setVisible(true);
+                reportRatButton.setVisible(true);
+                logoutButton.setVisible(true);
 
-                returnToDashButton.setVisibility(View.GONE);
-                applyFiltersButton.setVisibility(View.GONE);
+                returnToDashButton.setVisible(false);
+                applyFiltersButton.setVisible(false);
 
                 onMap = !onMap;
-                map.getUiSettings().setAllGesturesEnabled(onMap);
 
-                map.clear();
-
-                map.animateCamera(CameraUpdateFactory.newCameraPosition(position));
+                map = mapView.createMap(staticMapOptions);
+                map.setHeading(BEARING);
             }
         });
 
-        applyFiltersButton.setOnClickListener(v -> {
-            Intent intent = new Intent(v.getContext(), FilterActivity.class);
-            intent.putExtra(FilterActivity.FILTER, filter);
-            startActivityForResult(intent, FilterActivity.GET_FILTER);
+        applyFiltersButton.setOnAction(e -> {
+//            Intent intent = new Intent(v.getContext(), FilterActivity.class);
+//            intent.putExtra(FilterActivity.FILTER, filter);
+//            startActivityForResult(intent, FilterActivity.GET_FILTER);
         });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Log.d(TAG, "Received result from RatEntryActivity");
-        if (requestCode == FilterActivity.GET_FILTER) {
-            if (resultCode == RESULT_OK) {
-                filter = data.getParcelableExtra(FilterActivity.FILTER);
-                loadFilteredMapMarkers();
-            }
-        }
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        map = googleMap;
-        map.getUiSettings().setAllGesturesEnabled(onMap);
-        map.animateCamera(CameraUpdateFactory.newCameraPosition(position));
-        map.setInfoWindowAdapter(new CustomInfoWindowAdapter());
     }
 
     /**
@@ -212,50 +222,28 @@ public class DashMapActivity extends AppCompatActivity implements OnMapReadyCall
      * re-enabling all map widgets
      */
     private void loadFilteredMapMarkers() {
-        progressBar.setVisibility(View.VISIBLE);
-        returnToDashButton.setEnabled(false);
-        applyFiltersButton.setEnabled(false);
+        progressBar.setVisible(true);
+        returnToDashButton.setDisable(true);
+        applyFiltersButton.setDisable(true);
 
         List<RatData> filteredList = new RatDatabase().getFilteredRatData(filter);
-        map.clear();
+        map.clearMarkers();
         for (RatData r: filteredList) {
             MarkerOptions markerOptions = new MarkerOptions();
-            markerOptions.position(new LatLng(r.getLatitude(), r.getLongitude()));
+            markerOptions.position(new LatLong(r.getLatitude(), r.getLongitude()));
             markerOptions.title(r.getKey() + "");
-            markerOptions.snippet(r.getIncidentAddress() + ";" + r.getCreatedDateTime());
-            map.addMarker(markerOptions);
+            markerOptions.label(r.getIncidentAddress() + "\n" + r.getCreatedDateTime());
+            map.addMarker(new Marker(markerOptions));
         }
-        progressBar.setVisibility(View.GONE);
-        returnToDashButton.setEnabled(true);
-        applyFiltersButton.setEnabled(true);
+        progressBar.setVisible(false);
+        returnToDashButton.setDisable(false);
+        applyFiltersButton.setDisable(false);
     }
 
-    class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
-
-        private final View myContentsView;
-
-        CustomInfoWindowAdapter(){
-            myContentsView = getLayoutInflater().inflate(R.layout.rat_marker_view, null);
-        }
-
-        @Override
-        public View getInfoContents(Marker marker) {
-
-            TextView titleText = myContentsView.findViewById(R.id.rat_marker_view_title_text);
-            titleText.setText(marker.getTitle());
-            TextView snippetText1 = myContentsView.findViewById(R.id.rat_marker_view_snippet1_text);
-            TextView snippetText2 = myContentsView.findViewById(R.id.rat_marker_view_snippet2_text);
-            String[] splitSnippet = marker.getSnippet().split(";");
-            snippetText1.setText(splitSnippet[0]);
-            snippetText2.setText(splitSnippet[1]);
-
-            return myContentsView;
-        }
-
-        @Override
-        public View getInfoWindow(Marker marker) {
-            return null;
-        }
-
-    }
+	@Override
+	public void mapInitialized() {
+		map = mapView.createMap(staticMapOptions);
+		map.setHeading(BEARING);
+		mapButton.setDisable(false);
+	}
 }
